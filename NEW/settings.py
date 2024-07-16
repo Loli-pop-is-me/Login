b7f143d961a1570876045b75f3a3e4b4de40ecd3
Loli-pop-is-me <lolipop525563@gmail.com>
2024-07-16 12:27:48 +0530

    Update settings.py

--- NEW/settings.py
+++ NEW/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ui$oj3w$x)fm&l85+uu43e$gn-ws$7n!qjflhlr9bqah+9-bvj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
